922786b951ae95a3d1c128918e271056d200eace
Tordina <86804460+Tordina@users.noreply.github.com>
2022-06-09 18:18:43 +0200

    Add files

---
+++ 5_additional/add5.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <memory>
+#include <vector>
+//класс родителя
+class Foo 
+{
+private:
+    std::vector<int> m; //int* -> std::vector && i -> m
+
+public:
+    Foo(int j) 
+    {
+        std::cout << "Foo constructor" << std::endl;
+        for (int i = 0; i < j; i++)// <- i = new int[j]
+        { 
+            m.push_back(i);
+        }
+    }
+    virtual ~Foo()// добавлен virtual
+    { 
+        std::cout << "Foo destructor" << std::endl;
+        // удалено "delete i"
+    }
+    void get() // вывод 
+    { 
+        for (auto i : m) 
+        {
+            std::cout << m[i] << std::endl;
+        }
+    }
+};
+//класс предка
+class Bar : public Foo //добавлен public
+{ 
+private:
+    std::vector<char> k; //char* -> std::vector<char> && i ->k
+
+public:
+    Bar(int j) : Foo(j) // добавлено : Foo(j)
+    {
+        std::cout << "Bar constructor" << std::endl;
+        k.push_back((char)j); // <- i = new char[j]
+    }
+    virtual ~Bar() //добавлен virtual
+    { 
+        std::cout << "Bar destructor" << std::endl;
+        //удалено "delete i"
+    }
+
+};
+int main() { // void -> main
+
+    std::unique_ptr<Foo> f = std::make_unique<Foo>(5); // <- Foo* f=new Foo(100);
+    std::unique_ptr<Foo> b = std::make_unique<Bar>(2); // <- Foo* b=new Bar(200);
+
+    //добавлен вывод
+    std::cout << "Foo=" << std::endl;
+    f->get();
+    std::cout << "Bar=" << std::endl;
+    b->get();
+
+    *f = *b;
+
+    std::cout << "Foo=" << std::endl;
+    f->get();
+    std::cout << "Bar=" << std::endl;
+    b->get();
+};
+
